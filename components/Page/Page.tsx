ddd057f143a63f60c4cb38926bfcf0474d4da058
Abiel Zulio M <abielzm@gmail.com>
2022-12-28 23:34:49 +0700

    feat: add meta page title

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -1,3 +1,4 @@
+import Head from "components/Head"
 import React from "react"
 
 const Page = ({
@@ -8,12 +9,15 @@ const Page = ({
   title: string
 }) => {
   return (
-    <main className="w-screen h-screen bg-white text-black sm:p-[24px]">
-      <section className="max-w-[500px] h-full mx-auto sm:border-[1px] sm:border-[#ebebeb] sm:shadow-lg bg-white sm:rounded-xl md:p-[32px] p-[24px] flex flex-col gap-[24px]">
-        <h1 className="text-black text-[32px] font-bold">{title}</h1>
-        <section className="h-full">{children}</section>
-      </section>
-    </main>
+    <>
+      <Head title={title + " — Splitbill"} />
+      <main className="w-screen h-screen bg-white text-black sm:p-[24px]">
+        <section className="max-w-[500px] h-full mx-auto sm:border-[1px] sm:border-[#ebebeb] sm:shadow-lg bg-white sm:rounded-xl md:p-[32px] p-[24px] flex flex-col gap-[24px]">
+          <h1 className="text-black text-[32px] font-bold">{title}</h1>
+          <section className="h-full">{children}</section>
+        </section>
+      </main>
+    </>
   )
 }
 
