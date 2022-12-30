2c1d08ed19e0498fccbbda07a935a12f23f64d45
Abiel Zulio M <abielzm@gmail.com>
2022-12-31 00:02:25 +0700

    feat: add arrow left icon

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -59,3 +59,13 @@ export const New = (props: IconProps) => (
     />
   </Icon>
 )
+
+export const ArrowLeft = (props: IconProps) => (
+  <Icon {...props}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+    />
+  </Icon>
+)
