07bdf58442e2785e001d51d8e87d503310341044
Abiel Zulio M <abielzm@gmail.com>
2022-12-30 20:21:49 +0700

    style: move date bill item to the bottom

--- components/Bill/Item.tsx
+++ components/Bill/Item.tsx
@@ -20,66 +20,62 @@ export const BillItem = ({ bill }: { bill: Bill }) => {
       className="w-full flex items-start gap-[15px] hover:bg-gray-100 transition bg-white rounded-xl px-[12px] pt-[10px] pb-[15px]"
     >
       <Icon.Emoji>{bill.icon}</Icon.Emoji>
-      <div className="flex flex-col w-full">
-        <p className="text-[12px] opacity-50">
-          {new Date(bill.created_at).toLocaleDateString()}
-        </p>
-        <div className="flex justify-between w-full">
-          <div className="flex flex-col">
-            <p className="font-semibold">{bill.title}</p>
-            {billedPerson && (
-              <Avatar.Group
-                size="small"
-                className="mt-[5px]"
-                maxCount={12}
-                maxStyle={
-                  billedPerson[12]
-                    ? {
-                        backgroundColor: billedPerson[12].is_paid
-                          ? "#30c55e"
-                          : "#6b7280",
-                        color: "white",
-                      }
-                    : undefined
-                }
-              >
-                {billedPerson.map(({ name, is_paid, id, amount }) => (
-                  <Tooltip
-                    className="text-[10px]"
-                    key={id}
-                    title={`${name} ${
-                      is_paid ? "sudah" : "belum"
-                    } membayar Rp${amount.toLocaleString()}`}
-                    placement="bottomLeft"
-                    color={is_paid ? "#30c55e" : "#6b7280"}
-                    arrowPointAtCenter
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col">
+          <p className="font-semibold">{bill.title}</p>
+          {billedPerson && (
+            <Avatar.Group
+              size="small"
+              className="my-[5px]"
+              maxCount={12}
+              maxStyle={
+                billedPerson[12]
+                  ? {
+                      backgroundColor: billedPerson[12].is_paid
+                        ? "#30c55e"
+                        : "#6b7280",
+                      color: "white",
+                    }
+                  : undefined
+              }
+            >
+              {billedPerson.map(({ name, is_paid, id, amount }) => (
+                <Tooltip
+                  className="text-[10px]"
+                  key={id}
+                  title={`${name} ${
+                    is_paid ? "sudah" : "belum"
+                  } membayar Rp${amount.toLocaleString()}`}
+                  placement="bottomLeft"
+                  color={is_paid ? "#30c55e" : "#6b7280"}
+                  arrowPointAtCenter
+                >
+                  <Avatar
+                    className={`${
+                      is_paid ? `bg-green-500` : `bg-gray-500`
+                    } text-[8px]`}
                   >
-                    <Avatar
-                      className={`${
-                        is_paid ? `bg-green-500` : `bg-gray-500`
-                      } text-[8px]`}
-                    >
-                      {name
-                        .split(" ")
-                        .map((word) => word[0].toLocaleUpperCase())}
-                    </Avatar>
-                  </Tooltip>
-                ))}
-              </Avatar.Group>
-            )}
-          </div>
-          <div className="flex flex-col items-end gap-[5px]">
-            <p className="font-mono tracking-tight font-semibold">
-              {unpaidAmount
-                ? `Rp${unpaidAmount.toLocaleString()}`
-                : `Rp${bill.amount.toLocaleString()}`}
+                    {name.split(" ").map((word) => word[0].toLocaleUpperCase())}
+                  </Avatar>
+                </Tooltip>
+              ))}
+            </Avatar.Group>
+          )}
+          <p className="text-[12px] opacity-50">
+            {new Date(bill.created_at).toLocaleDateString()}
+          </p>
+        </div>
+        <div className="flex flex-col items-end gap-[5px]">
+          <p className="font-mono tracking-tight font-semibold">
+            {unpaidAmount
+              ? `Rp${unpaidAmount.toLocaleString()}`
+              : `Rp${bill.amount.toLocaleString()}`}
+          </p>
+          {!bill.is_paid && (
+            <p className="font-mono text-[12px] opacity-50 tracking-tight">
+              Rp{bill.amount.toLocaleString()}
             </p>
-            {!bill.is_paid && (
-              <p className="font-mono text-[12px] opacity-50 tracking-tight">
-                Rp{bill.amount.toLocaleString()}
-              </p>
-            )}
-          </div>
+          )}
         </div>
       </div>
     </div>
